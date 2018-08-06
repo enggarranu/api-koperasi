d2c1932ce674c91b7242f392640cf4ab6c5bc974
enggar.ranu <enggar.ranu@gmail.com>
2018-08-06 14:57:56 +0700

    Login Management | inquiry petugas

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ def delete_petugas():
             db = connection.get_db()
             curr = db.cursor()
 
-            q_modify = ("UPDATE `db_koperasi`.`tb_ms_login` set `flag_active` = FALSE WHERE `id` = '"+id_petugas+"';")
+            q_modify = ("UPDATE `db_koperasi`.`tb_ms_login` set `flagactive` = FALSE WHERE `id` = '"+id_petugas+"';")
             print (q_modify)
             curr.execute(q_modify)
             db.commit()
