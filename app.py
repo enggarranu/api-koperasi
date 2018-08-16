46210fb9204d8013329ac9ead41ee3f55ca868ca
enggar.ranu <enggar.ranu@gmail.com>
2018-08-16 11:59:24 +0700

    Bayar Cicilan

--- app.py
+++ app.py
@@ -597,6 +597,36 @@ def inquiry_pengambilan():
             db.close()
             return json.dumps(res_data)
 
+
+# PEMBAYARAN_CICILAN
+@app.route('/get_id_transaksi_pembayaran', methods=["GET",])
+def get_id_transaksi_pembayaran():
+        res_data = {}
+        if request.method == 'GET':
+            db = connection.get_db()
+            curr = db.cursor()
+            q_is_exist = (
+                        "SELECT count(id_pembayaran) as jumlah FROM `tb_pembayaran`;")
+            curr.execute(q_is_exist)
+            rs = curr.fetchall()
+            jumlah_row = rs[0][0]
+            res_data['response'] = 'OK'
+            res_data['msg'] = 'UTI/PBYRN'+str(datetime.datetime.today().strftime('%Y%m%d'))+str(jumlah_row+1)
+
+            q_is_exist = ("SELECT\n" +
+"	ta.id_anggota, ta.nama_anggota, kr.id_kredit, id_pengambilan\n" +
+"FROM\n" +
+"	`tb_kredit` kr\n" +
+"	LEFT JOIN tb_pembayaran pb ON kr.id_kredit = pb.id_kredit\n" +
+"	JOIN tb_anggota ta on ta.id_anggota = kr.id_anggota\n" +
+"WHERE\n" +
+"	kr.id_pengambilan IS NOT NULL")
+            curr.execute(q_is_exist)
+            rs = curr.fetchall()
+            res_data['anggota_arr'] = rs
+            db.close()
+            return json.dumps(res_data)
+
 if __name__ == '__main__':
     handler = RotatingFileHandler('/var/log/api-koperasi/API_KOPERASI.log', maxBytes=10000, backupCount=1)
     handler.setLevel(logging.INFO)
