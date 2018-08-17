7dc208dab2877a678e9483be03015fc5cec3053f
enggar.ranu <enggar.ranu@gmail.com>
2018-08-18 01:24:35 +0700

    Add function inquiry_pembayaran

--- app.py
+++ app.py
@@ -727,11 +727,11 @@ def bayar_cicilan():
                 "	( '"+id_pembayaran+"', '"+id_anggota+"', '"+tanggal_pembayaran+"', "+denda+", '"+tanggal_tempo_pembayaran+"', "+jumlah_pembayaran+", "+sisa_pinjaman+", '"+id_kredit+"', "+angsuran_ke+", "+sisa_angsuran+", '"+insert_by+"' )")
         curr.execute(q)
         if int(sisa_angsuran) != 0 :
-            lunas = 0
+            lunas = False
         else :
-            lunas = 1
+            lunas = True
 
-        q_update = ("update tb_kredit set sisa_pinjaman = "+sisa_pinjaman+", sisa_angsuran = "+sisa_angsuran+", lunas = '"+lunas+"' where id_kredit = '"+id_kredit+"'")
+        q_update = ("update tb_kredit set sisa_pinjaman = "+sisa_pinjaman+", sisa_angsuran = "+sisa_angsuran+", lunas = "+lunas+" where id_kredit = '"+id_kredit+"'")
         curr.execute(q_update)
         db.commit()
         res_data = {}
